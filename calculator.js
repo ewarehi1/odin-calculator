eca7bc09456e5b181331266f6c6b9d1f189f286b
ewarehi1 <128170875+ewarehi1@users.noreply.github.com>
2023-11-25 14:28:17 -0700

    Create functions for operators

--- calculator.js
+++ calculator.js
@@ -4,9 +4,25 @@ const buttons = document.querySelectorAll('.number, .operator');
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-        console.log(button.textContent)
+
         display.textContent += button.textContent;
     });
 })
 
-const submit = document.querySelector('#submit')
+const submit = document.querySelector('#submit');
+
+function add(initial, addend) {
+    return initial + addend;
+}
+
+function subtract(initial, subtrahend) {
+    return initial - subtrahend;
+}
+
+function multiply(initial, multiplier) {
+    return initial * multiplier;
+}
+
+function divide(initial, divisor) {
+    return initial / divisor;
+}
